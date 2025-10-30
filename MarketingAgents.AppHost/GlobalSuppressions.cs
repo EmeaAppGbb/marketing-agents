22f674f9797adcffca1513454970a89e87512f62
Kosta Petan <kostapetan@gmail.com>
2025-10-30 12:19:10 +0100

    cleaunp

--- MarketingAgents.AppHost/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Marketing Agents. All rights reserved.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA0001:XML comment analysis is disabled", Justification = "Not required for all files")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Not required for all files")]
-[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1516:Elements should be separated by blank line", Justification = "AppHost configuration has tight grouping")]
